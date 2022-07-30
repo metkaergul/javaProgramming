03deca7160a257d13a10107d331fdc6a44d7f0d0
metkaergul <metkaergul@gmail.com>
2022-07-30 16:13:26 +0300

    day38 inheritance

---
+++ src/day38_Inheritance/ShapeTask/Shape.java
@@ -0,0 +1,35 @@
+package day38_Inheritance.ShapeTask;
+
+public class Shape {
+
+}
+/*Shape:
+	variable:
+		name
+
+	Add A constructor to set the filed
+
+	Methods:
+		area(){return 0}
+		perimeter{return 0}
+		toString(): name, area, perimeter
+
+
+Circle
+	r
+	pi
+
+	toString(): name, r, pi, area, perimeter
+
+Square
+	s
+
+	toString(): name, s, area, perimeter
+
+Rectangle
+	l
+	w
+
+	toString(): name, l, w, area, perimeter
+
+....*/
