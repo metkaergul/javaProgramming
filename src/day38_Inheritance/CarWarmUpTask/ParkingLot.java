753b388fe3f64b9113b87132312e4da6d723021c
metkaergul <metkaergul@gmail.com>
2022-07-30 14:55:07 +0300

    day38 inheritance

--- src/day38_Inheritance/CarWarmUpTask/ParkingLot.java
+++ src/day38_Inheritance/CarWarmUpTask/ParkingLot.java
@@ -6,6 +6,7 @@ public class ParkingLot {
         Toyota toyota=new Toyota("Camry",2020,20000,"Gray",123456);
 
         Tesla tesla= new Tesla("Model S",2022,94990,"Red",0);
+        //has a relationship betwween parking lot class and tesla and toyota class
 
 
     }
