38688e3e05da781a16642be7b33427a4f63b0ee9
metkaergul <metkaergul@gmail.com>
2022-07-30 14:52:50 +0300

    day38 inheritance

---
+++ src/day38_Inheritance/CarWarmUpTask/ParkingLot.java
@@ -0,0 +1,12 @@
+package day38_Inheritance.CarWarmUpTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+
+        Toyota toyota=new Toyota("Camry",2020,20000,"Gray",123456);
+
+        Tesla tesla= new Tesla("Model S",2022,94990,"Red",0);
+
+
+    }
+}
