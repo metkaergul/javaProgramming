01f64735d45f36c25aa2371b23f25964b42a7f7f
metkaergul <metkaergul@gmail.com>
2022-07-31 12:54:38 +0300

    day39:recap

---
+++ src/day39_Recap/Circle.java
@@ -0,0 +1,56 @@
+package day39_Recap;
+
+public class Circle extends Shape{
+
+    private double radius;
+    public static double pi=3.14;
+
+    public Circle( double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius<=0){
+            System.err.println("radius can not be zero or negative");
+            System.exit(1);
+        }
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return pi*radius*radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*pi*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                "pi=" + pi +
+                "area=" + area() +
+                "perimeter=" + perimeter() +
+                '}';
+    }
+}
+/*Circle extends Shape:
+		variables:
+			radius
+			pi (static)
+
+		Encapsulate the field
+
+		Add a constructor to set the filed
+
+		area(): radius * radius * pi
+		perimeter(): 2 * radius * pi
+		toString(): r, pi, area, perimeter*/
--- src/day39_Recap/Shape.java
+++ src/day39_Recap/Shape.java
@@ -39,6 +39,9 @@ public class Shape {
     public String toString() {
         return "Shape{" +
                 "name='" + name + '\'' +
+                "area='" + area() + '\'' +
+                "perimeter='" + perimeter() + '\'' +
+
                 '}';
     }
 }
