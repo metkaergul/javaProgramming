71900e3e9918c98a32e0d82afa571b5cd2c048cd
metkaergul <metkaergul@gmail.com>
2022-07-31 12:32:54 +0300

    day39:recap

---
+++ src/day39_Recap/Shape.java
@@ -0,0 +1,44 @@
+package day39_Recap;
+
+import day31_Constructors.RestaurantTask.Server;
+
+import java.rmi.ServerError;
+
+public class Shape {
+    private String name;
+
+
+    public Shape(String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        if(name==null){
+            System.err.println("name can not be null");
+            System.exit(1);
+        }
+        if(name.isEmpty()||name.isBlank()){
+            System.err.println("invalid name entry");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public double area(){
+        return 0;
+    }
+    public double perimeter(){
+        return 0;
+    }
+
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/day39_Recap/Square.java
@@ -0,0 +1,40 @@
+package day39_Recap;
+
+public class Square extends Shape{
+
+    private double side;
+
+    public Square(int side) {
+        super("Square");
+        setSide(side);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        if(side<=0){
+            System.err.println("side of the square can not be zero or negative");
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+    public double area() {
+        return side*side;
+    }
+
+    public double perimeter() {
+        return 4*side;
+    }
+
+    public String toString() {
+        return "Square{" +
+
+                "side=" + side +
+                "area='" + area() + '\'' +
+                "perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+}
