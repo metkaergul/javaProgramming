8aa9097ddba6895edc61d01076fc98568914c8c4
metkaergul <metkaergul@gmail.com>
2022-08-24 18:13:35 +0300

    browser task done

--- src/day45_Abstraction/Browser/DriverObjects.java
+++ src/day45_Abstraction/Browser/DriverObjects.java
@@ -13,6 +13,22 @@ public class DriverObjects {
         safaridriver.getBrowserName();
         safaridriver.takeScreenShot();
 
+        WebDriver driver1= new ChromeDriver("chrome");
+        driver1.getTitle();
+        driver1.close();
+        driver1.findElement("cssSelector");
+        System.out.println("driver1 = " + driver1);
+
+        WebDriver driver2= new SafariDriver("safari");
+        driver2.findElement("xpath");
+        driver2.quit();
+        driver2.close();
+
+        WebDriver driver3= new FirefoxDriver("firefox");
+        driver3.close();
+        System.out.println("driver3 = " + driver3);
+        driver3.getTitle();
+
 
     }
 }
