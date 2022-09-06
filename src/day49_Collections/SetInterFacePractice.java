318cc9e676c29654797a266fa9def734a8cac766
metkaergul <metkaergul@gmail.com>
2022-09-06 12:28:58 +0300

    setinterface class is created

--- src/day49_Collections/SetInterFacePractice.java
+++ src/day49_Collections/SetInterFacePractice.java
@@ -6,12 +6,12 @@ public class SetInterFacePractice {
     public static void main(String[] args) {
 
 
-        List<Integer>list= new ArrayList<>();
-        list.addAll(Arrays.asList(10,200,300,40,90));
-        list.addAll(Arrays.asList(10,200,300,40,90));
-        list.addAll(Arrays.asList(10,200,300,40,90));
-        list.addAll(Arrays.asList(10,200,300,40,90));
-        list.addAll(Arrays.asList(10,200,300,40,90));
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
 
         System.out.println("list = " + list);
 
@@ -19,28 +19,42 @@ public class SetInterFacePractice {
 
         System.out.println("__________________________");
 
-        Set<Integer>hashSet= new HashSet<>();
+        Set<Integer> hashSet = new HashSet<>();
 
-            hashSet.addAll(Arrays.asList(10,200,300,40,90));
-            hashSet.addAll(Arrays.asList(10,200,300,40,90));
-            hashSet.addAll(Arrays.asList(10,200,300,40,90));
-            hashSet.addAll(Arrays.asList(10,200,300,40,90));
-            hashSet.addAll(Arrays.asList(null,null,null));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(null, null, null));
         System.out.println("hashSet = " + hashSet);
 
         //System.out.println(hashSet.get(4));
 
-        Set<Integer> linkedHashSet= new LinkedHashSet<>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(null, null, null));
+
+        System.out.println("linkedHashSet = " + linkedHashSet);
 
 
-        Set<Integer>treeSet= new TreeSet<>();
+        Set<Integer> treeSet = new TreeSet<>();
+
+
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+       // treeSet.addAll(Arrays.asList(null,null,null));
 
 
 
+        System.out.println("treeSet = " + treeSet);
 
     }
-
-
 
 
 }
---
+++ src/day49_Collections/SetPractice.java
@@ -0,0 +1,5 @@
+package day49_Collections;
+
+public class SetPractice {
+
+}
